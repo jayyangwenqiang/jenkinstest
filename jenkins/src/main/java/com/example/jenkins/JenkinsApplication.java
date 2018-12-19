0861e7582f7e0eaedafecd866457ad15ccf0cf7c
Jay <1072894762@qq.com>
2018-12-19 21:18:18 +0800

    第二次提36

--- jenkins/src/main/java/com/example/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -3,8 +3,11 @@ package com.example.jenkins;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RestController
 @SpringBootApplication
 public class JenkinsApplication {
 
@@ -12,6 +15,12 @@ public class JenkinsApplication {
 		SpringApplication.run(JenkinsApplication.class, args);
 		log.info("项目启动完成!");
 	}
+
+
+	@GetMapping("hi")
+	public String greet(){
+	    return "hi,greeting";
+    }
 
 }
 
